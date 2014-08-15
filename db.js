91161be4e8feeb0e448a2434cd0b8c0e5ec203a4
Samuel Christie <emailium@gmail.com>
2014-08-15 10:39:18 -0400

    Remove flatten and add rest of the collection names

--- db.js
+++ db.js
@@ -3,15 +3,27 @@ var Promise = require('bluebird');
 var mongojs = Promise.promisifyAll(require('mongojs'));
 
 var url = "eve:api service access@localhost/socaster";
-var collections = ["users", "tools", "recommendations", "user_tools"];
+var collections = [
+  "applications",
+  "clips",
+  "copy_tools",
+  "events",
+  "fs.chunks",
+  "fs.files",
+  "images",
+  "log",
+  "notifications",
+  "ratings",
+  "recommendations",
+  "system.indexes",
+  "system.users",
+  "test",
+  "tools",
+  "usages",
+  "user_tools",
+  "users",
+  "yammer_tokens"
+];
 var db = mongojs.connect(url, collections);
-
-db.flatten = function(collection) {
-  return collection.findAsync().map(function(item) {
-    if (item.value) {
-      collection.update({_id: item._id}, item.value);
-    }
-  }).all();
-}
 
 module.exports = db;
