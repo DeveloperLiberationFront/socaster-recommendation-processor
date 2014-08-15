f446b980752d0addb79118ddbdacb894c06c16d8
Samuel Christie <emailium@gmail.com>
2014-08-15 10:39:48 -0400

    Add logging to process-recommendations.sh

--- process-recommendations.sh
+++ process-recommendations.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-CMD_REC=command-recommender
-LOG_SETTINGS=$CMD_REC/log4j2.xml
-JAR=$CMD_REC/commandrecommender-1.0-SNAPSHOT.jar
-COMMAND_TABLE=events
+DBNAME="socaster"
 USER="eve"
 PASSWORD="api service access"
-DBNAME="socaster"
+COMMAND_TABLE=events
 DETAILS_TABLE=tools
+
+CMD_REC=command-recommender
+JAR=$CMD_REC/commandrecommender-1.0-SNAPSHOT.jar
+LOG_SETTINGS=log4j2.xml
+
+ACCEPTANCE=MULTI_USE
 declare -a ALGORITHM_TYPES=("MOST_FREQUENTLY_USED" "MOST_WIDELY_USED" "HOTKEY_NOT_USED" "MOST_POPULAR_LEARNING_RULE" "USER_BASED_CF" "USER_BASED_CF_WITH_DISCOVERY" "LEARNING_RULE" "MOST_PREREQ_LEARNING_RULE" "ITEM_BASED_CF_WITH_DISCOVERY" "ITEM_BASED_CF" "LATENT_MODEL_BASED_CF")
 
 function pre {
@@ -23,7 +26,7 @@ function post {
 
 # NOTE: This requires GNU getopt.  On Mac OS X and FreeBSD, you have to install this
 # separately; see below.
-OPTS=`getopt -o vd --long debug,verbose -n 'process-recommendations' -- "$@"`
+OPTS=`getopt -o vd --long debug,verbose,log -n 'process-recommendations' -- "$@"`
 
 # Exit if there were errors processing args
 if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
@@ -32,10 +35,12 @@ if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 eval set -- "$OPTS"
 VERBOSE=false
 DEBUG=false
+LOG=false
 
-#process arguments
+# Process arguments
 while true; do
   case "$1" in
+    -l | --log ) LOG=true; shift ;;
     -v | --verbose ) VERBOSE=true; shift ;;
     -d | --debug ) DEBUG=true; shift ;;
     -- ) shift; break ;;
@@ -43,10 +48,18 @@ while true; do
   esac
 done
 
-#conditionally redirect output to /dev/null
+# Conditionally redirect output to /dev/null
 if $DEBUG || $VERBOSE
 then exec 3>&1;
 else exec 3>/dev/null;
+fi
+
+# Test if running via tty or not. If so, enable logging
+tty -s
+if [ $? != 0 ] ; then LOG=true; fi
+if $LOG
+then
+    exec >process.log 2>process.log
 fi
 
 pre
@@ -60,6 +73,7 @@ do
         -cpass "${PASSWORD}" \
         -cn "${DBNAME}" \
         --command_detail_table "${DETAILS_TABLE}" \
+        -c $ACCEPTANCE\
         -t $type \
         $OPTIONS
 done
